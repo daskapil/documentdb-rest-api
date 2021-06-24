11a5ebad1deaf8d86eb6d04c9a72fb91b0f18003
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2021-06-24 14:12:49 +0000

    UPdated the shell script

--- scripts/TrustStoreCACert.sh
+++ scripts/TrustStoreCACert.sh
@@ -1,6 +1,8 @@
 mydir=/tmp/certs
+mkdir ${mydir}
+
 truststore=${mydir}/rds-truststore.jks
-storepassword=<truststorePassword>
+storepassword=truststorePassword
 
 curl -sS "https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem" > ${mydir}/rds-combined-ca-bundle.pem
 awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1}{print > "rds-ca-" n ".pem"}' < ${mydir}/rds-combined-ca-bundle.pem
